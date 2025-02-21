45b34777df9e9215206d8e7e5b9269f9659c5d04
Trung Nam <trungnam.work@gmail.com>
2025-02-22 00:16:13 +0700

    MongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -16,6 +16,10 @@ app.use(
 )
 
 app.use(express.json());
+
+
+connectDB();
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
