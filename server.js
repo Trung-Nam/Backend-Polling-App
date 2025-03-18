61e70df5c600c418abdf69da96632cc4be018fc5
Trung Nam <trungnam.work@gmail.com>
2025-03-18 15:09:23 +0700

    Upload Image API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/authRoutes");
 
@@ -24,6 +25,10 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth",authRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 
 const PORT = process.env.PORT || 5000;
