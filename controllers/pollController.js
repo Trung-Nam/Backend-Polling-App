427d26f1d43660c2b46cd99608d96cd0048117fe
Trung Nam <trungnam.work@gmail.com>
2025-08-13 21:28:35 +0700

    feat: Get Poll By ID API

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -220,8 +220,16 @@ exports.getVotedPolls = async (req, res) => {
 }
 // Get polls by ID
 exports.getPollById = async (req, res) => {
+    const { id } = req.params;
     try {
+        const poll = await Poll.findById(id)
+            .populate("creator", "username email");
 
+        if (!poll) {
+            return res.status(404).json({ message: "Poll not found" });
+        }
+
+        res.status(200).json(poll);
     } catch (error) {
         res
             .status(500)
